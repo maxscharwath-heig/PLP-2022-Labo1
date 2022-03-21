90e09e9663a29ad3d468e2a2e4780fb51b15142f
nicrausaz <n.crausaz99@gmail.com>
2022-03-21 17:11:04 +0100

    Roman type

--- roman.hs
+++ roman.hs
@@ -8,3 +8,19 @@
 -}
 module Roman where
 
+data RomanNumber = O | I | V | X | L | C | D | M 
+   deriving(Show)
+
+numberToRoman :: Integer -> RomanNumber
+numberToRoman 0 = O
+numberToRoman 1 = I
+numberToRoman 5 = V
+numberToRoman 10 = X
+numberToRoman 50 = L
+numberToRoman 100 = C
+numberToRoman 500 = D
+numberToRoman 1000 = M
+-- numberToRoman n = 
+
+
+
