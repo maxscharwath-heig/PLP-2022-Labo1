3ade498621396482c4821ac7359ae20b2e8352f8
nicrausaz <n.crausaz99@gmail.com>
2022-04-18 14:00:57 +0200

    Ex 3 need fix

--- natural.hs
+++ natural.hs
@@ -31,10 +31,9 @@ instance Num Nat where
    -- (^) S n S m = n * m
    -- (^) _  _ = Z 
 
-   fromInteger 0 = Z
+   fromInteger 0 = Z -- Pas vraiment juste (retourne S Z et on veut Z)
+   fromInteger 1 = Z
    fromInteger n = S (fromInteger (n-1))
-
-
 
 instance Eq Nat where
    -- ==
@@ -44,5 +43,46 @@ instance Eq Nat where
 
    -- /=
    Z /= Z = True
-   S n /= S m = not (S n == S m)
+   S n /= S m = n /= m
    _ /= _ = True
+
+instance Ord Nat where
+   -- <
+   Z < S n = True
+   S n < Z = False
+   S n < S m = n < m
+   _ < _ = False
+
+   -- <=
+   Z <= S n = True
+   S n <= Z = False
+   S n <= S m = n <= m
+   _ <= _ = True
+
+   -- >
+   Z > S n = False
+   S n > Z = True
+   S n > S m = n > m
+   _ > _ = False
+
+   -- >=
+   Z >= S n = False
+   S n >= Z = True
+   S n >= S m = n >= m
+   _ >= _ = True
+
+instance Enum Nat where 
+   succ Z = S 1
+   succ (S n) = S (n + 1)
+
+   pred Z = Z
+   pred (S n) = S (n - 1)
+
+
+
+zero :: Nat 
+zero = Z
+
+isZero :: Nat -> Bool
+isZero Z = True 
+isZero _ = False
