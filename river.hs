e275976ffeefc7a13da1f53e708d09534720e1a5
Nicolas Crausaz <nicrausaz@users.noreply.github.com>
2022-04-20 11:43:43 +0200

    Start of ex 5, help function

--- river.hs
+++ river.hs
@@ -1,3 +1,4 @@
+import System.Environment (getArgs)
 {-
    PLP - Laboratoire 1
 
@@ -6,4 +7,33 @@
    @author Nicolas Crausaz
    @author Maxime Scharwath
 -}
-module River where
+
+commands =
+  [ (":p", helpCallback, "afficher l'état du jeu"),
+    (":l", helpCallback, "charger la barque avec un passager"),
+    (":u", helpCallback, "décharger la barque"),
+    (":m", helpCallback, "déplacer la barque"),
+    (":r", helpCallback, "réinitialiser le jeu"),
+    (":q", helpCallback, "quitter le jeu"),
+    (":h", helpCallback, "afficher l'aide")
+  ]
+
+
+getCommand :: String -> [(String, IO (), String)]
+getCommand arg = filter (\(x, _, _) -> x == arg) commands
+
+helpCallback = do putStrLn "Commandes disponibles :"
+                  mapM_ (\(x, _, d) -> putStrLn $ "  " ++ x ++ " " ++ d) commands
+
+main = do
+   args <- getArgs
+   if null args then error "Invalid command"
+   else do 
+      let command = getCommand (head args)
+      if null command then error "Invalid command"
+      else do
+      let params = tail args
+
+      -- get callback
+      let (_, callback, _) = head command
+      callback
